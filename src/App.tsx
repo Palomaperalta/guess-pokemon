74d10f1803571e16eff623dba9625ce37d21fe79
Paloma Peralta <palomaperalta9@gmail.com>
2022-08-31 19:09:40 -0300

    segunda entrega

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
   const [pokemon, setPokemon] = useState<Pokemon | null>(null)
   const [pokemonName, setPokemonName] = useState('')
   const [imgClassName, setImgClassName] = useState('noadivino')
+  const [openDialog, setOpenDialog] = useState(false)
 
   useEffect(()=>{
     getPokemon()
@@ -16,21 +17,37 @@ function App() {
     setPokemon(newpokemon)
   } 
   const handleAnswer = () =>{
-    if(pokemonName.toLowerCase() === pokemon.name ){
+    if(pokemonName.toLowerCase().replace(/\s/g, "").replace(/\./g, "").trim() === pokemon.name){
       setImgClassName('adivino')
+      setOpenDialog(true)
     }
   }
   const handleNameChange = (event) =>{
     setPokemonName(event.target.value)
   }
+  const handlePlayAgain = () =>{
+    window.location.reload()
+  }
+
   return (
     <main>
       <span id="titulo" className="nes-text is-primary">Quién es este Pokemon?</span>
       <img className={imgClassName} alt="" src={pokemon?.image}></img>
       <div className="nes-field" id="answer">
         <input value={pokemonName} onChange={handleNameChange} type="text" id="name_field" className="nes-input border" />
-        <button onClick={handleAnswer} type="button" className="nes-btn is-primary border">Adivinar</button>
+        <button onClick={handleAnswer} type="button" className={`nes-btn is-primary border${imgClassName === 'adivino' ? ' is-disabled': ''}`}>Adivinar</button>
+        {imgClassName === 'adivino' ? <button onClick={handlePlayAgain} type="button" className="nes-btn is-success">Play again</button> : null}
       </div>
+      <dialog open={openDialog} className="nes-dialog" id="dialog-default">
+        <form method="dialog">
+          <p className="title">Congratulations!</p>
+          <p>You answered correctly</p>
+          <menu className="dialog-menu">
+            <button className="nes-btn">Cancel</button>
+            <button onClick={handlePlayAgain} className="nes-btn is-primary">Play again</button>
+          </menu>
+        </form>
+      </dialog>
     </main>
   )
 }
